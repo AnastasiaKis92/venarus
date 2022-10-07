6ee84a0f4cf7ccc8054c88d8df130ba0718ee3fe
Anastasia Kiseleva <aakkiseleva@gmail.com>
2022-10-07 18:41:44 +0300

    Блок production

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,6 +1,7 @@
 import {addBurger} from '../blocks/burger/burger.js';
 import {addTooltip} from '../blocks/benefits/benefits.js';
 
+import '../blocks/production/production.js';
 
 addBurger();
 addTooltip();
